33eeb51325e2927e5859ff55f8109e2f726a4741
tungnd5 <tungnd5@revex.vn>
2021-11-29 09:26:10 +0700

    fix session expire

--- routes/web.php
+++ routes/web.php
@@ -24,28 +24,20 @@ Auth::routes();
 
 Route::get('/home', [
     HomeController::class, 'index'
-])->name('home');
+])->name('home')->middleware('auth');
+Route::middleware(['auth'])->group(function () {
+    Route::resource('menus', App\Http\Controllers\MenuController::class);
+    Route::resource('rawMaterialImports', App\Http\Controllers\RawMaterialImportController::class);
+    Route::resource('checkoutOrders', App\Http\Controllers\CheckoutOrderController::class);
+    Route::resource('notes', App\Http\Controllers\NoteController::class);
+    Route::resource('customers', App\Http\Controllers\CustomerController::class);
+    Route::post('checkoutOrders/create-note',[\App\Http\Controllers\CheckoutOrderController::class,'createNote'])->name('create-note');
+    Route::post('checkoutOrders/update-note',[\App\Http\Controllers\CheckoutOrderController::class,'updateNote'])->name('update-note');
+    Route::get('checkoutOrders/search',[\App\Http\Controllers\CheckoutOrderController::class,'search'])->name('search');
+    Route::post('get-menu-price',[\App\Http\Controllers\CheckoutOrderController::class,'getMenuPrice'])->name('get-menu-price');
+    Route::get('toggle-status',[\App\Http\Controllers\CheckoutOrderController::class,'toggleStatus'])->name('toggle-status');
+    Route::get('spending-export/', [\App\Http\Controllers\RawMaterialImportController::class,'export'])->name('spending-export');
+    Route::get('bill-export', [\App\Http\Controllers\CheckoutOrderController::class,'export'])->name('bill-export');
+});
 
 
-Route::resource('menus', App\Http\Controllers\MenuController::class);
-
-Route::resource('rawMaterialImports', App\Http\Controllers\RawMaterialImportController::class);
-
-Route::resource('rawMaterialImports', App\Http\Controllers\RawMaterialImportController::class);
-
-Route::post('checkoutOrders/create-note',[\App\Http\Controllers\CheckoutOrderController::class,'createNote'])->name('create-note');
-Route::post('checkoutOrders/update-note',[\App\Http\Controllers\CheckoutOrderController::class,'updateNote'])->name('update-note');
-Route::get('checkoutOrders/search',[\App\Http\Controllers\CheckoutOrderController::class,'search'])->name('search');
-Route::post('get-menu-price',[\App\Http\Controllers\CheckoutOrderController::class,'getMenuPrice'])->name('get-menu-price');
-Route::get('toggle-status',[\App\Http\Controllers\CheckoutOrderController::class,'toggleStatus'])->name('toggle-status');
-
-Route::resource('checkoutOrders', App\Http\Controllers\CheckoutOrderController::class);
-
-
-Route::resource('notes', App\Http\Controllers\NoteController::class);
-
-
-Route::resource('customers', App\Http\Controllers\CustomerController::class);
-
-Route::get('spending-export/', [\App\Http\Controllers\RawMaterialImportController::class,'export'])->name('spending-export');
-Route::get('bill-export', [\App\Http\Controllers\CheckoutOrderController::class,'export'])->name('bill-export');
