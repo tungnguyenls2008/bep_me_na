ee69350714864d2dc90fe02ce9ebcbfa1b7f7285
tungnd5 <tungnd5@revex.vn>
2021-12-15 11:34:42 +0700

    doing permission

--- backend.sql
+++ backend.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th12 01, 2021 lúc 11:31 AM
+-- Thời gian đã tạo: Th12 15, 2021 lúc 05:32 AM
 -- Phiên bản máy phục vụ: 10.4.21-MariaDB
 -- Phiên bản PHP: 7.4.23
 
@@ -55,6 +55,15 @@ CREATE TABLE `industry` (
   `deleted_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Đang đổ dữ liệu cho bảng `industry`
+--
+
+INSERT INTO `industry` (`id`, `name`, `description`, `created_at`, `updated_at`, `deleted_at`) VALUES
+(1, 'May mặc - Thời trang', 'Sed tempora saepe aut quo molestiae animi laboris esse', '2021-12-09 04:00:29', '2021-12-13 09:23:31', NULL),
+(2, 'Nhà hàng - Ẩm thực', 'Aliqua Sint nihil saepe qui amet tempora in quasi distinctio Consequatur autem officia exercitationem ullam', '2021-12-09 04:00:34', '2021-12-13 09:23:52', NULL),
+(3, 'Phụ tùng - Cơ khí', 'Assumenda officiis itaque temporibus ipsum officia ea velit', '2021-12-09 04:00:38', '2021-12-13 09:24:38', NULL);
+
 -- --------------------------------------------------------
 
 --
@@ -65,10 +74,12 @@ CREATE TABLE `organization` (
   `id` int(11) NOT NULL,
   `profile_id` int(11) DEFAULT NULL,
   `name` varchar(255) NOT NULL,
-  `ceo_id` int(11) NOT NULL,
-  `licence` varchar(24) NOT NULL,
+  `logo` varchar(255) DEFAULT NULL,
+  `ceo_id` int(11) DEFAULT NULL,
+  `licence` varchar(36) NOT NULL,
   `db_name` varchar(24) NOT NULL,
   `status` int(11) NOT NULL DEFAULT 0 COMMENT '0->trial\r\n1->active\r\n2->lock',
+  `lock_status` int(11) NOT NULL DEFAULT 0 COMMENT '0-active\r\n1-lock',
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
   `updated_at` timestamp NULL DEFAULT NULL,
   `deleted_at` timestamp NULL DEFAULT NULL
@@ -111,6 +122,7 @@ CREATE TABLE `product` (
 CREATE TABLE `profile` (
   `id` int(11) NOT NULL,
   `name` varchar(255) NOT NULL,
+  `organization_id` int(11) NOT NULL,
   `ceo_id` int(11) DEFAULT NULL,
   `product_ids` varchar(500) DEFAULT NULL,
   `industry_id` int(11) DEFAULT NULL,
@@ -122,6 +134,32 @@ CREATE TABLE `profile` (
   `updated_at` timestamp NULL DEFAULT NULL,
   `deleted_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `role`
+--
+
+CREATE TABLE `role` (
+  `id` int(11) NOT NULL,
+  `route` varchar(255) NOT NULL,
+  `description` varchar(500) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NULL DEFAULT NULL,
+  `deleted_at` timestamp NULL DEFAULT NULL,
+  `status` int(11) NOT NULL DEFAULT 0 COMMENT '0=>active\r\n1=>lock'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Đang đổ dữ liệu cho bảng `role`
+--
+
+INSERT INTO `role` (`id`, `route`, `description`, `created_at`, `updated_at`, `deleted_at`, `status`) VALUES
+(6, '[\"menu-toggle-status\",\"menus.index\",\"menus.create\",\"menus.store\",\"menus.show\",\"menus.edit\",\"menus.update\",\"menus.destroy\"]', 'Toàn quyền quản lý sản phẩm', '2021-12-15 03:04:34', '2021-12-15 03:04:34', NULL, 0),
+(7, '[\"menus.index\"]', 'Xem danh sách sản phẩm', '2021-12-15 03:06:22', '2021-12-15 03:06:22', NULL, 0),
+(8, '[\"home\"]', 'Xem trang chủ (quyền mặc định)', '2021-12-15 03:33:21', '2021-12-15 03:33:21', NULL, 0),
+(9, '[\"checkoutOrders.index\",\"checkoutOrders.create\",\"checkoutOrders.store\",\"checkoutOrders.show\",\"checkoutOrders.edit\",\"checkoutOrders.update\",\"checkoutOrders.destroy\"]', 'Toàn quyền quản lý doanh thu', '2021-12-15 04:12:17', '2021-12-15 04:12:17', NULL, 0);
 
 -- --------------------------------------------------------
 
@@ -188,6 +226,12 @@ ALTER TABLE `profile`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Chỉ mục cho bảng `role`
+--
+ALTER TABLE `role`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Chỉ mục cho bảng `users`
 --
 ALTER TABLE `users`
@@ -208,7 +252,7 @@ ALTER TABLE `ceo`
 -- AUTO_INCREMENT cho bảng `industry`
 --
 ALTER TABLE `industry`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT cho bảng `organization`
@@ -227,6 +271,12 @@ ALTER TABLE `product`
 --
 ALTER TABLE `profile`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT cho bảng `role`
+--
+ALTER TABLE `role`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT cho bảng `users`
--- core_db.sql
+++ core_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th12 14, 2021 lúc 11:02 AM
+-- Thời gian đã tạo: Th12 15, 2021 lúc 05:34 AM
 -- Phiên bản máy phục vụ: 10.4.21-MariaDB
 -- Phiên bản PHP: 7.4.23
 
